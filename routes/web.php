ce56459fa662658d4873ea0aadd3b41c51a1d855
mathiconical <netinho340@gmail.com>
2024-06-16 11:38:15 -0300

    create a route to login request

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/login', [UserController::class, 'login']);
